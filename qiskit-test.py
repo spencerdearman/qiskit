2ca1766297096e4a82428ce2b3e3737d21f77e93
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-11-04 19:50:12 -0500

    Finished initial tutorial, now going to play with superstaq

--- qiskit-test.py
+++ qiskit-test.py
@@ -1,60 +1,24 @@
 from qiskit import *
 import matplotlib.pyplot as plt
-from qiskit.visualization import plot_state_city
-from qiskit.tools.monitor import job_monitor
-from qiskit.tools.visualization import plot_histogram
-from qiskit.test.reference_circuits import ReferenceCircuits
-from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
+from qiskit.visualization import plot_state_city, plot_histogram
+from qiskit.visualization import plot_bloch_multivector, plot_state_hinton
+from qiskit.visualization import plot_state_paulivec, plot_state_qsphere
  
 # # Save an IBM Quantum account and set it as your default account.
-# QiskitRuntimeService.save_account(channel="ibm_quantum", token="6ab51482612c0c9f8e09ec7430f42a53681f6260f9909f31577e9cc1154316d0c182f336fe1499da352e6d910d16cf427d57fcbe0a3fbb225d373a713cd1b242", set_as_default=True)
+# QiskitRuntimeService.save_account(channel="ibm_quantum", token="<TOKEN>", 
+# set_as_default=True)
 
-
-# # Part I
-
-# # Load saved credentials
-# service = QiskitRuntimeService()
-
-# # build 2 qubit quantum register
-# qr = QuantumRegister(2)
-
-# # building 2 classical bit classical register
-# cr = ClassicalRegister(2)
-
-# # quantum circuit
-# circuit = QuantumCircuit(qr, cr)
-
-# # hadamard gate 
-# circuit.h(qr[0])
-
-# # controlled X gate
-# circuit.cx(qr[0], qr[1])
-
-# # measure qubits and put into classical register
-# circuit.measure(qr, cr)
-
-# # simulate quantum circuit -- using air
-# simulator = Aer.get_backend('qasm_simulator')
-# result = execute(circuit, backend = simulator).result()
-
-# plotting results from simulator 
-# plot_histogram(result.get_counts(circuit))
-# circuit.draw(output='mpl') 
-# plt.show()
-
-
-# Part II
-
+# Part I ---------------------------------------------------------------------
 # Create a Quantum Circuit acting on a quantum register of three qubits
 circ = QuantumCircuit(3)
 
 # Add a H gate on qubit $q_{0}$, putting this qubit in superposition.
 circ.h(0)
-# Add a CX (CNOT) gate on control qubit $q_{0}$ and target qubit $q_{1}$, putting
-# the qubits in a Bell state.
+# Add a CX (CNOT) gate on control qubit $q_{0}$ and target qubit $q_{1}$, 
+# putting the qubits in a Bell state.
 circ.cx(0, 1)
-# Add a CX (CNOT) gate on control qubit $q_{0}$ and target qubit $q_{2}$, putting
-# the qubits in a GHZ state.
+# Add a CX (CNOT) gate on control qubit $q_{0}$ and target qubit $q_{2}$, 
+# putting the qubits in a GHZ state.
 circ.cx(0, 2)
 circ.draw('mpl')
 
@@ -69,16 +33,46 @@ result = job.result()
 outputstate = result.get_statevector(circ, decimals=3)
 print(outputstate)
 
+# Real and imaginary components of the state density matrix 
 plot_state_city(outputstate)
+
+# Part II --------------------------------------------------------------------
+# Quantum circuit to make a Bell state
+bell = QuantumCircuit(2, 2)
+bell.h(0)
+bell.cx(0, 1)
+
+# Measuring circuit
+meas = QuantumCircuit(2, 2)
+meas.measure([0,1], [0,1])
+
+# Execute the quantum circuit
+backend = BasicAer.get_backend('qasm_simulator') # the device to run on
+circ = bell.compose(meas)
+result = backend.run(transpile(circ, backend), shots=1000).result()
+counts  = result.get_counts(circ)
+print(counts)
+plot_histogram(counts)
+
+# Execute 2-qubit Bell state again
+second_result = backend.run(transpile(circ, backend), shots=1000).result()
+second_counts  = second_result.get_counts(circ)
+# Plot results with legend
+legend = ['First execution', 'Second execution']
+plot_histogram([counts, second_counts], legend=legend)
+# plt.show()
+
+# Part III -------------------------------------------------------------------
+# Different types of plots available 
+# - plot_state_city(quantum_state)
+# - plot_state_qsphere(quantum_state)
+# - plot_state_paulivec(quantum_state)
+# - plot_state_hinton(quantum_state)
+# - plot_bloch_multivector(quantum_state)
+# execute the quantum circuit
+
+backend = BasicAer.get_backend('statevector_simulator') # the device to run on
+result = backend.run(transpile(bell, backend)).result()
+psi  = result.get_statevector(bell)
+plot_bloch_multivector(psi)
 plt.show()
-
-# # You'll need to specify the credentials when initializing QiskitRuntimeService, if they were not previously saved.
-# backend = service.backend("ibmq_qasm_simulator")
-# job = Sampler(backend).run(circuit)
-# print(f"job id: {job.job_id()}")
-# job_monitor(job)
-# q_result = job.result()
-# # counts = q_result.data().get('counts')
-# plot_histogram(q_result.quasi_dists[0])
-# # print(dir(q_result))
-
